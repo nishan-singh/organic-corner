77c063c2ff4b4b173320b9cfd66b7c6955054b07
Nishan Singh <singh007nishan@gmail.com>
2023-11-21 12:41:04 +0100

    Refactor new-products component to use dynamic data

--- src/app/components/new-products/new-products.component.ts
+++ src/app/components/new-products/new-products.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+type Product = {
+  name: string;
+  price: number;
+  img: string;
+  desc: string;
+};
 @Component({
   selector: 'app-new-products',
   templateUrl: './new-products.component.html',
-  styleUrls: ['./new-products.component.scss']
+  styleUrls: ['./new-products.component.scss'],
 })
 export class NewProductsComponent {
-
+  products: Product[] = [
+    {
+      name: 'Strawberries',
+      price: 4.99,
+      img: '/assets/img/product-img/strawberries.jpg',
+      desc: 'lorem ipsum ',
+    },
+    {
+      name: 'Strawberries',
+      price: 4.99,
+      img: '/assets/img/product-img/strawberries.jpg',
+      desc: 'lorem ipsum ',
+    },
+    {
+      name: 'Strawberries',
+      price: 4.99,
+      img: '/assets/img/product-img/strawberries.jpg',
+      desc: 'lorem ipsum ',
+    },
+    {
+      name: 'Strawberries',
+      price: 4.99,
+      img: '/assets/img/product-img/strawberries.jpg',
+      desc: 'lorem ipsum ',
+    },
+  ];
 }
