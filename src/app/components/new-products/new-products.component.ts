144be144b27a4ba3be2047609f71efbecc1cbcd7
Nishan Singh <singh007nishan@gmail.com>
2023-12-01 15:50:26 +0100

    Refactor new-products component to use productsList

--- src/app/components/new-products/new-products.component.ts
+++ src/app/components/new-products/new-products.component.ts
@@ -1,41 +1,18 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
+import { ProductsService } from 'src/app/services/products.service';
 
-type Product = {
-  name: string;
-  price: number;
-  img: string;
-};
 @Component({
   selector: 'app-new-products',
   templateUrl: './new-products.component.html',
   styleUrls: ['./new-products.component.scss'],
 })
 export class NewProductsComponent {
-  products: Product[] = [
-    {
-      name: 'Strawberries',
-      price: 6.99,
-      img: '/assets/img/product-img/strawberries.jpg',
-    },
-    {
-      name: 'Maize',
-      price: 4.49,
-      img: '/assets/img/product-img/maize.jpg',
-    },
-    {
-      name: 'Broccoli',
-      price: 9.99,
-      img: '/assets/img/product-img/broccoli.jpg',
-    },
-    {
-      name: 'Paprika',
-      price: 5.99,
-      img: '/assets/img/product-img/paprika.jpg',
-    },
-    {
-      name: 'Tomatoes',
-      price: 3.99,
-      img: '/assets/img/product-img/tomatoes.jpg',
-    },
-  ];
+  productsList?: Product[];
+
+  constructor(private products: ProductsService) {
+    this.productsList = this.products.getProducts();
+    console.log("help...");
+
+  }
 }
