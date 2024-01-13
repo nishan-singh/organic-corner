17f19f7e27d5a7fcd9e58fa410c206ed071103e9
Nishan Singh <singh007nishan@gmail.com>
2024-01-13 18:15:48 +0100

    add 'how it works' component to hero com

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -4,6 +4,7 @@ import { NewProductsComponent } from '../new-products/new-products.component';
 import { WeeklyOfferComponent } from '../weekly-offer/weekly-offer.component';
 import { DeliveryProcessSectionComponent } from '../delivery-process-section/delivery-process-section.component';
 import { FooterComponent } from '../footer/footer.component';
+import { HowItWorksComponent } from '../how-it-works/how-it-works.component';
 
 @Component({
   selector: 'app-hero',
@@ -12,10 +13,11 @@ import { FooterComponent } from '../footer/footer.component';
     OfferSectionComponent,
     NewProductsComponent,
     WeeklyOfferComponent,
+    HowItWorksComponent,
     DeliveryProcessSectionComponent,
     FooterComponent,
   ],
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss'],
 })
-export class HeroComponent {}
+export class HeroComponent { }
