ed63c19ce62f92b855a203f9a67f036cb49ac4cf
Nishan Singh <singh007nishan@gmail.com>
2024-01-13 18:16:39 +0100

    make the component standalone

--- src/app/components/how-it-works/how-it-works.component.ts
+++ src/app/components/how-it-works/how-it-works.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-how-it-works',
+  standalone: true,
   templateUrl: './how-it-works.component.html',
-  styleUrls: ['./how-it-works.component.scss']
+  styleUrls: ['./how-it-works.component.scss'],
 })
-export class HowItWorksComponent {
-
-}
+export class HowItWorksComponent { }
