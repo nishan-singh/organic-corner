b96eabe17570bd9e3c37e2b314191ac048b671e7
Nishan Singh <singh007nishan@gmail.com>
2024-01-31 13:30:44 +0100

    add logic for each product details

--- src/app/components/buy-product/buy-product.component.ts
+++ src/app/components/buy-product/buy-product.component.ts
@@ -1,3 +1,4 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/interfaces/product';
@@ -5,17 +6,19 @@ import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-buy-product',
+  standalone: true,
   templateUrl: './buy-product.component.html',
   styleUrls: ['./buy-product.component.scss'],
+  imports: [CurrencyPipe],
 })
 export class BuyProductComponent {
   productsDetails?: Product;
 
   constructor(
     private route: ActivatedRoute,
-    private productsService: ProductsService
+    private productsService: ProductsService,
   ) {
-    const id = +this.route.snapshot.params['id']; // + converts string to number [+(string)
+    const id = Number(this.route.snapshot.params['id']);
     this.productsDetails = this.productsService.getProductDetailsById(id);
     console.log(this.productsDetails);
   }
